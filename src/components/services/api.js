db36b294906fb405c0cf55226bf16f62c82286a2
Yashgupta@8707 <yashgupta@spinfluencemedia.com>
2025-05-10 13:56:56 +0530

    uiweh

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://server-pkfp.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
